f5ff1e7d75b3fe733788a1f71df5f981b66c40f6
BDENOYELLE <boris.denoyelle@afp.com>
2025-10-23 13:23:27 +0200

    add test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goosarch
+
+go 1.24.3
--- main.go
+++ main.go
@@ -7,36 +7,61 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 )
 
+type Job struct {
+	GoOS, GoArch string
+}
 
-func getList() (map[string][]string) {
-  out, err := exec.Command("go", "tool", "dist", "list").Output()
-    if err != nil {
-        panic(err)
-    }
-    lines := strings.Split(strings.TrimSpace(string(out)), "\n")
-    platforms := make(map[string][]string)
-    for _, line := range lines {
-        parts := strings.Split(line, "/")
-        if len(parts) != 2 {
-            continue // ignore les lignes mal formées
-        }
-        os := parts[0]
-        arch := parts[1]
-        platforms[os] = append(platforms[os], arch)
-    }
+func getList() map[string][]string {
+	out, err := exec.Command("go", "tool", "dist", "list").Output()
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(strings.TrimSpace(string(out)), "\n")
+	platforms := make(map[string][]string)
+	for _, line := range lines {
+		parts := strings.Split(line, "/")
+		if len(parts) != 2 {
+			continue
+		}
+		os := parts[0]
+		arch := parts[1]
+		platforms[os] = append(platforms[os], arch)
+	}
 	return platforms
 }
 
 func main() {
 	osArch := getList()
+	
+	jobs := make(chan Job)
+	var wg sync.WaitGroup
+
+	numWorkers := 1 // Trois workers fixes
+
+	// Lancement des workers
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func(workerID int) {
+			defer wg.Done()
+			for job := range jobs {
+				fmt.Printf("Worker %d compile %s/%s\n", workerID, job.GoOS, job.GoArch)
+				buildFor(job.GoOS, job.GoArch)
+			}
+		}(i + 1)
+	}
+
+	// Remplissage de la queue de jobs
 	for os, archs := range osArch {
-		
 		for _, arch := range archs {
-			buildFor(os, arch)
+			jobs <- Job{GoOS: os, GoArch: arch}
 		}
 	}
+
+	close(jobs)
+	wg.Wait()
 }
 
 func buildFor(goos, goarch string) {
@@ -59,7 +84,7 @@ func buildFor(goos, goarch string) {
 
 	if err := cmd.Run(); err != nil {
 		log.Printf("❌ Erreur compilation %s/%s : %v", goos, goarch, err)
+	} else {
+		fmt.Printf("✅ Binaire créé : %s\n\n", output)
 	}
-
-	fmt.Printf("✅ Binaire créé : %s\n\n", output)
 }
